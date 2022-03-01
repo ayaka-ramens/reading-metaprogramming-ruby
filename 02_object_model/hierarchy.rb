c6e36b2fe402ce7b0be249e6c3ed8b9f6e150f94
Ayaka Moronaga <a.moronaga@everyleaf.com>
2022-03-01 09:01:07 +0900

    chapter2

--- 02_object_model/hierarchy.rb
+++ 02_object_model/hierarchy.rb
@@ -30,33 +30,53 @@ end
 # - C1.ancestors.first(2) が [C1, M1] となる
 # - C1.new.name が 'C1' を返す
 class C1
+  include M1
   def name
     'C1'
   end
 end
 
+C1.ancestors.first(2) #=> [C1, M1]
+C1.new.name #=> 'C1'
 
 # Q2.
 # 次の動作をする C2 class を実装する
 # - C2.ancestors.first(2) が [M1, C2] となる
 # - C2.new.name が 'M1' を返す
 class C2
+  prepend M1
   def name
     'C2'
   end
 end
 
+C2.ancestors.first(2) # [M1, C2]
+C2.new.name # 'M1'
 
 # Q3.
 # 次の動作をする C3 class, MySuperClass class を実装する
 # - C3.ancestors.first(6) が [M1, C3, M2, M3, MySuperClass, M4] となる
 # - C3.new.name が 'M1' を返す
-class C3
+class MySuperClass
+  include M4
+  prepend M3
+
+  def name
+    'MySuperClass'
+  end
+end
+
+class C3 < MySuperClass
+  include M2
+  prepend M1
+
   def name
     'C3'
   end
 end
 
+C3.ancestors.first(6) #=> [M1, C3, M2, M3, MySuperClass, M4]
+C3.new.name #=> 'M1'
 
 # Q4.
 # 次の動作をする C4 class のメソッド increment を実装する
@@ -68,10 +88,24 @@ end
 #   c4.increment # => "3"
 # - 定義済みのメソッド (value, value=) は private のままとなっている
 class C4
+  def initialize
+    @value = 0
+  end
+
+  def increment
+    self.value = value + 1
+    value.to_s
+  end
+
   private
 
   attr_accessor :value
 end
+
+c4 = C4.new
+c4.increment # => "1"
+c4.increment # => "2"
+c4.increment # => "3"
 
 # Q5.
 # 次の動作をする M1Refinements module を実装する
@@ -80,6 +114,11 @@ end
 # - C5.new.another_name が文字列 "M1" を返す
 # - C5.new.other_name が文字列 "Refined M1" を返す
 module M1Refinements
+  refine M1 do
+    def name
+      "Refined M1"
+    end
+  end
 end
 
 class C5
@@ -96,6 +135,14 @@ class C5
   end
 end
 
+module M1
+  def name
+    "M1"
+  end
+end
+
+C5.new.another_name #=> "M1"
+C5.new.other_name #=> "Refined M1"
 
 # Q6.
 # 次の動作をする C6 class を実装する
@@ -104,4 +151,10 @@ end
 class C6
   include M1
   using M1Refinements
+
+  def name
+    super
+  end
 end
+
+p C6.new.name #=> 'Refined M1'
--- 02_object_model/hoge.rb
+++ 02_object_model/hoge.rb
@@ -1,7 +1,29 @@
-class Hoge
+class Hoge < String
   # Hogeクラスの仕様
   # "hoge" という文字列の定数Hogeを持つ
   # "hoge" という文字列を返すhogehogeメソッドを持つ
   # HogeクラスのスーパークラスはStringである
   # 自身が"hoge"という文字列である時（HogeクラスはStringがスーパークラスなので、当然自身は文字列である）、trueを返すhoge?メソッドが定義されている
+  HOGE = 'hoge'
+
+  def initialize(str)
+    @str = str
+  end
+
+  def hogehoge
+    'hoge'
+  end
+
+  def hoge?
+    @str == 'hoge' ? true : false
+  end
 end
+
+h = Hoge.new('hoge')
+puts h.hogehoge
+puts h.hoge?
+puts "定数HOGE: #{Hoge::HOGE}"
+puts '========================'
+f = Hoge.new('fuga')
+puts f.hogehoge
+puts f.hoge?
